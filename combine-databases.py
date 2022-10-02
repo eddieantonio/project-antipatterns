06aa5edc07a0b5cd0ba3dd31d90a874a54c1c019
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-10-02 12:29:32 +0100

    feat: create combine-databases

---
+++ combine-databases.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Combine all the per-slice errors-*.sqlite3 databases into errors.sqlite3
+"""
+
+import sqlite3
+from pathlib import Path
+
+from database import SCHEMA
+
+dbs = list(Path(".").glob("errors-*.sqlite3"))
+assert len(dbs) > 0
+
+# Initit the combined db with the schema first:
+conn = sqlite3.connect("errors.sqlite3")
+with conn:
+    conn.executescript(SCHEMA)
+
+
+# Based on https://stackoverflow.com/a/11089277
+for db_path in dbs:
+    conn.executescript(f"""
+        ATTACH '{db_path}' as slice;
+        BEGIN;
+        INSERT INTO messages
+            SELECT * FROM slice.messages;
+        COMMIT;
+        DETACH slice;
+    """)
