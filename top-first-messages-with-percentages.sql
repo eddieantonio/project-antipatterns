39397c52164dd2fdfe6ab09278ff857aa407a18f
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-10-07 23:01:24 +0100

    feat: add SQL for top first messages with percentages

---
+++ top-first-messages-with-percentages.sql
@@ -0,0 +1,9 @@
+SELECT sanitized_text,
+       COUNT(sanitized_text) AS occurrences,
+	   100.0 * COUNT(sanitized_text) / (SELECT COUNT(*) FROM messages WHERE rank = 1) as percentage,
+       SUM(100.0 * COUNT(sanitized_text) / (SELECT COUNT(*) FROM messages WHERE rank = 1)) OVER
+         (ORDER BY COUNT(sanitized_text) DESC) as cummulative_percentage
+  FROM messages JOIN sanitized_messages USING (text)
+ WHERE rank = 1
+ GROUP BY sanitized_text
+ ORDER BY COUNT(sanitized_text) DESC;
