838a2d7bc6851f798c57fa82b06e85c31724410b
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-10-07 21:17:10 +0100

    [wip]: create Database class

--- error_database/__init__.py
+++ error_database/__init__.py
@@ -2,13 +2,148 @@
 Shared utilties for the database.
 """
 
+import sqlite3
+from functools import lru_cache
+from pathlib import PurePosixPath
+from typing import Iterator
+
+from java_error_messages import match_message
+
 SCHEMA = """
-CREATE TABLE IF NOT EXISTS messages(
-    srcml_path TEXT,
-    version INT,
-    rank INT,
-    start TEXT,
-    end TEXT,
-    text TEXT
+CREATE TABLE all_messages(
+    srcml_path      TEXT,
+    version         INT NOT NULL,
+    rank            INT NOT NULL,
+    start           TEXT NOT NULL,
+    end             TEXT NOT NULL,
+    text            TEXT NOT NULL,
+
+    PRIMARY KEY (srcml_path, version, rank)
 );
+
+CREATE TABLE source(
+    srcml_path      TEXT PRIMARY KEY,
+    slice           TEXT NOT NULL,
+    project_id      INT NOT NULL,
+    source_file_id  INT NOT NULL
+);
+
+CREATE TABLE sanitized_messages(
+    text            TEXT PRIMARY KEY,
+    javac_name      TEXT NOT NULL,
+    sanitized_text  TEXT NOT NULL,
+);
+
+CREATE VIEW first_messages(srcml_path, version, start, end, text)
+AS SELECT srcml_path, version, start, end, text
+     FROM all_messages;
 """
+
+
+Message = tuple[str, int, int, str, str, str]
+
+
+class Database:
+    """
+    Provides an abstracted mechanism to the error message database.
+    """
+
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+        self._register_helpers()
+
+    def ensure_initialized(self) -> None:
+        """
+        Make sure all tables and views are created.
+        """
+        with self.conn:
+            self.conn.executescript(SCHEMA)
+
+    def insert_batch(self, messages: Iterator[Message]):
+        """
+        Inserts a batch of error messages into the database. All messages are inserted
+        in one transaction. It's recommened to insert to insert an entire project's
+        worth at a time, so that if anything fails, it can be localized to one specific
+        project.
+        """
+        with self.conn:
+            self.conn.executemany(
+                """
+                INSERT INTO all_messages (srcml_path, version, rank, start, end, text)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """,
+                messages,
+            )
+
+    def populate_sanitized_messages(self) -> None:
+        """
+        Generates sanitized messages from all messages.
+        """
+        with self.conn:
+            self.conn.execute(
+                """
+                INSERT INTO sanitized_messages (text, javac_name, sanitized_text)
+                AS SELECT text,
+                          javac_name(text) as javac_name,
+                          sanitize_message(text) as sanitized_text
+                     FROM (SELECT DISTINCT text from all_messages)
+                """
+            )
+
+    def populate_sources(self) -> None:
+        """
+        Generates sources from all messages.
+        """
+        with self.conn:
+            self.conn.execute(
+                """
+                INSERT INTO source(srcml_path, slice, project_id, source_id)
+                AS SELECT srcml_path,
+                          slice_name(srcml_path) as slice,
+                          project_id(srcml_path) as project_id,
+                          source_id(srcml_path) as source_id
+                     FROM all_messages
+                """
+            )
+
+    def _register_helpers(self):
+        conn.create_function("sanitize_message", 1, sanitize_message)
+        conn.create_function("javac_name", 1, javac_name)
+        conn.create_function("slice_name", 1, slice_name)
+        conn.create_function("project_id", 1, project_id)
+        conn.create_function("source_id", 1, source_id)
+
+
+# Cache match_message() to avoid too many re lookups.
+match_message = lru_cache(1024)(match_message)
+
+
+def sanitize_message(text: str) -> str:
+    return match_message(text).sanitized_message
+
+
+def javac_name(text: str) -> str | None:
+    return match_message(text).javac_name
+
+
+def slice_name(text: str) -> str:
+    return parse_path(PurePosixPath(text))[0]
+
+
+def project_id(text: str) -> int:
+    return parse_path(PurePosixPath(text))[1]
+
+
+def source_id(text: str) -> int:
+    return parse_path(PurePosixPath(text))[2]
+
+
+@lru_cache(128)
+def parse_path(path: PurePosixPath):
+    *_, slice_dir, project_dir, xml_name = path.parts
+    assert xml_name.endswith(".xml")
+    slice_name = slice_dir.removeprefix("srcml-")
+    project_id = project_dir.removeprefix("project-")
+    source_id = path.stem.removeprefix("src-")
+
+    return slice_name, int(project_id), int(source_id)
